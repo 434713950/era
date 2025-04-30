6933b25d333363cfb3db97ca8b17157b7e598c19
到另一个你的身边去 <434713950@qq.com>
2025-04-30 17:07:24 +0800

    1.调整TMS存储key值机制

--- era-framework-tms/era-framework-tms-redission/src/main/java/com/ourexists/era/framework/tms/EraTmsRedisAutoConfiguration.java
+++ era-framework-tms/era-framework-tms-redission/src/main/java/com/ourexists/era/framework/tms/EraTmsRedisAutoConfiguration.java
@@ -3,6 +3,7 @@ package com.ourexists.era.framework.tms;
 import com.ourexists.era.framework.tms.core.manager.EraThirdAccessTokenManger;
 import com.ourexists.era.framework.tms.core.requester.RemoteTokenRequester;
 import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
@@ -11,7 +12,7 @@ public class EraTmsRedisAutoConfiguration {
 
     @Bean
     public EraThirdAccessTokenManger eraThirdAccessTokenManger(
-            Redisson redisson,
+            RedissonClient redisson,
             List<RemoteTokenRequester> remoteTokenRequester) {
         return new RedisThirdAccessTokenManager(redisson, remoteTokenRequester);
     }
