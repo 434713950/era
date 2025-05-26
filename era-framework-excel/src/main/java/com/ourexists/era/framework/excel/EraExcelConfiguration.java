309b9d2c8023090a601b68dcee2201ff198cd0bf
到另一个你的身边去 <434713950@qq.com>
2025-05-26 17:52:15 +0800

    1. springbatch默认不使用线程池，最好由上层实现，方便线程方法传递

--- era-framework-excel/src/main/java/com/ourexists/era/framework/excel/EraExcelConfiguration.java
+++ era-framework-excel/src/main/java/com/ourexists/era/framework/excel/EraExcelConfiguration.java
@@ -2,12 +2,20 @@ package com.ourexists.era.framework.excel;
 
 import com.ourexists.era.framework.excel.export.ExcelTaskerConfigurer;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.sql.DataSource;
 import java.util.Map;
 
+@EnableBatchProcessing
 @Import(ExcelTaskerConfigurer.class)
 public class EraExcelConfiguration {
 
@@ -15,5 +23,24 @@ public class EraExcelConfiguration {
     public ExcelJobExecutor excelJobExecutor(JobLauncher jobLauncher,
                                              Map<String, Job> jobMaps) {
         return new ExcelJobExecutor(jobLauncher, jobMaps);
+    }
+
+    @Bean
+    public JobRepository jobRepository(DataSource dataSource,
+                                       PlatformTransactionManager transactionManager) throws Exception {
+        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+        factory.setDataSource(dataSource);
+        factory.setTransactionManager(transactionManager);
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+
+    @Bean
+    public JobLauncher jobLauncher(JobRepository jobRepository) throws Exception {
+        SimpleJobLauncher launcher = new SimpleJobLauncher();
+        launcher.setJobRepository(jobRepository);
+        launcher.afterPropertiesSet();
+        return launcher;
     }
 }
--- era-framework-excel/src/main/java/com/ourexists/era/framework/excel/export/ExcelTaskerConfigurer.java
+++ era-framework-excel/src/main/java/com/ourexists/era/framework/excel/export/ExcelTaskerConfigurer.java
@@ -7,16 +7,15 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import java.util.Map;
 
 @Slf4j
-public class ExcelTaskerConfigurer implements BeanDefinitionRegistryPostProcessor {
+public class ExcelTaskerConfigurer implements InitializingBean {
 
     private JobBuilderFactory jobBuilderFactory;
 
@@ -26,29 +25,35 @@ public class ExcelTaskerConfigurer implements BeanDefinitionRegistryPostProcesso
 
     private OssTemplate ossTemplate;
 
+    private ConfigurableApplicationContext context;
+
+
     public ExcelTaskerConfigurer(JobBuilderFactory jobBuilderFactory,
                                  StepBuilderFactory stepBuilderFactory,
                                  Map<String, ExportDataHandler> exportExecutorMaps,
-                                 OssTemplate ossTemplate) {
+                                 OssTemplate ossTemplate,
+                                 ConfigurableApplicationContext context) {
         this.jobBuilderFactory = jobBuilderFactory;
         this.stepBuilderFactory = stepBuilderFactory;
         this.exportExecutorMaps = exportExecutorMaps;
         this.ossTemplate = ossTemplate;
+        this.context = context;
     }
 
     @Override
-    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+    public void afterPropertiesSet() throws Exception {
+        BeanDefinitionRegistry registry = (BeanDefinitionRegistry) context.getBeanFactory();
         for (Map.Entry<String, ExportDataHandler> entry : exportExecutorMaps.entrySet()) {
             ExportDataHandler exportDataHandler = entry.getValue();
-            Step one = stepBuilderFactory.get("era_exportInitStep")
+            Step one = stepBuilderFactory.get("era_export_init_step_" + entry.getKey())
                     .tasklet(new ExportInitTasklet(exportDataHandler))
                     .listener(new ExportInitTaskletListener(exportDataHandler))
                     .build();
-            Step two = stepBuilderFactory.get("era_exportRwStep")
+            Step two = stepBuilderFactory.get("era_export_rw_step_" + entry.getKey())
                     .tasklet(new ExportRwTasklet(exportDataHandler))
                     .listener(new ExportRwTaskletListener(exportDataHandler))
                     .build();
-            Step three = stepBuilderFactory.get("era_exportUploadStep")
+            Step three = stepBuilderFactory.get("era_export_upload_step_" + entry.getKey())
                     .tasklet(new ExportUploadOssTasklet(exportDataHandler, ossTemplate))
                     .listener(new ExportUploadOssTaskletListener(exportDataHandler))
                     .build();
@@ -60,10 +65,5 @@ public class ExcelTaskerConfigurer implements BeanDefinitionRegistryPostProcesso
                             .build());
             registry.registerBeanDefinition(ExportJobKey.JOB_BEAN_PREFIX + entry.getKey(), jobBeanBuilder.getBeanDefinition());
         }
-    }
-
-    @Override
-    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-
     }
 }
